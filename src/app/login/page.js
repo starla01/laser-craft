0b4e59600bac84487d96976b68350ad9ef605e21
Fernando <fernando@MacBook-Pro-de-Fernando.local>
2023-04-22 09:39:38 -0600

    third-production

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,6 @@
 'use client'
 
 import Box from '@mui/material/Box'
-import Input from '@mui/material/Input'
-import InputLabel from '@mui/material/InputLabel'
-import InputAdornment from '@mui/material/InputAdornment'
-import FormControl from '@mui/material/FormControl'
 import TextField from '@mui/material/TextField'
 import AccountCircle from '@mui/icons-material/AccountCircle'
 import PasswordIcon from '@mui/icons-material/Password'
